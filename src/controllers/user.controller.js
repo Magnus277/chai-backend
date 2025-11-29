5252df471644e2ead3675650412e0e900b9949cd
Debayan <ddebayan29@gmail.com>
2025-11-29 19:49:42 +0530

    fix: await User.findByIdAndUpdate in logout and updateAccountDetails functions

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -144,7 +144,7 @@ const loginUser = asyncHandler(async (req,res) => {
 
 
 const logoutUser= asyncHandler(async(req,res) => {
-    User.findByIdAndUpdate(
+    await User.findByIdAndUpdate(
       req.user._id,
       {
         $set: {
@@ -230,7 +230,7 @@ const updateAccountDetails = asyncHandler(async(req,res)=> {
       throw new ApiError(400, "All fields are required")
     }
 
-    const user = User.findByIdAndUpdate(
+    const user = await User.findByIdAndUpdate(
       req.user?._id,
       {
         $set: {
